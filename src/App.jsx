5fb2f540563f16d9b74b46160dcf12f41a6dc9a3
Gevorg Petrosyan <petrossian.gev@gmail.com>
2026-05-11 11:33:31 +0400

    adding props and default props to reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
-
+import Row from "./ui/Row";
 
 const StyledApp = styled.main`
   background-color: var(--color-grey-100);
@@ -15,12 +15,28 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as = "h1">The Wild Oasis</Heading>
-         <Heading as = "h2">The Wild Oasis</Heading>
-          <Heading as = "h3">The Wild Oasis</Heading>
-        <Button>Check in</Button>
-        <Button>Check out</Button>
-        <Input type="number" placeholder="Number of guests" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("Check in clicked!")}>
+                Check in
+              </Button>
+              <Button variation="secondary" size="small">
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
